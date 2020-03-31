1cf93882b3bd8e5de855bf85bd6917c13610886d
Jorge Ramirez <joramirez96@gmail.com>
2020-03-31 22:47:38 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,24 +15,14 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts = {
-  "Jon Snow" => {
-    name: "Jon Snow",
-    email: "jon_snow@thewall.we",
-    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]}
-    }
-
-    contacts.each do |attribute, data|
+  contacts.each do |attribute, data|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
-        binding.pry
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
-end
+end 
+
