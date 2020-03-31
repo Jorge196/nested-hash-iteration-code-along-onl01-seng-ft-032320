6cd71aa87a1672fb71c0749a44cc34e337d95eff
Jorge Ramirez <joramirez96@gmail.com>
2020-03-31 22:33:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -27,6 +27,7 @@ def remove_strawberry(contacts)
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]}
     }
   end
+end 
 
 def remove_strawberry(contacts)
   contacts.each do |attribute, data|
